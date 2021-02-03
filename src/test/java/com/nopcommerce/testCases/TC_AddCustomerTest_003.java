e986410d5e293157b242fd9a74ccc113f846a0a2
Manoj <Manojdograaa@gmail.com>
2021-02-03 10:56:01 +0530

    Modified results

---
+++ src/test/java/com/nopcommerce/testCases/TC_AddCustomerTest_003.java
@@ -0,0 +1,90 @@
+package com.nopcommerce.testCases;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.nopcommerce.pageObjects.AddcustomerPage;
+import com.nopcommerce.pageObjects.loginPage;
+
+public class TC_AddCustomerTest_003 extends baseClass
+{
+
+	@Test
+	public void addNewCustomer() throws IOException, InterruptedException
+	{
+		driver.get(baseURL);
+		
+		loginPage lp=new loginPage(driver);
+		lp.setUsername(username);
+		logger.info("User provided"); //logger msg
+		
+		lp.setPassword(password);
+		logger.info("Password provided");//logger msg
+		
+		lp.clickLogin();
+		logger.info("Login in Clicked");//logger msg
+		
+		logger.info("providing customer details....");
+		
+		AddcustomerPage addcust=new AddcustomerPage(driver);
+		
+		addcust.clickOnCustomersMenu();
+		addcust.clickOnCustomersMenuItem();
+		
+		addcust.clickOnAddnew();
+		
+		String email = randomestring() + "@gmail.com";
+		addcust.setEmail(email);
+				
+		addcust.setPassword("test123");
+		
+		//Registered - default
+		//The customer cannot be in both 'Guests' and 'Registered' customer roles
+		//Add the customer to 'Guests' or 'Registered' customer role
+		addcust.setCustomerRoles("Guest");
+		
+		addcust.setManagerOfVendor("Vendor 2");
+		
+		addcust.setGender("Male");
+		
+		addcust.setFirstName("Pavan");
+		addcust.setLastName("Kumar");
+		
+		addcust.setDob("7/05/1985"); // Format: D/MM/YYY
+		
+		addcust.setCompanyName("busyQA");
+		addcust.setAdminContent("This is for testing.........");
+	
+		addcust.clickOnSave();
+		
+		logger.info("validation started....");
+				
+		//String msg=driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissable']")).getText();
+		
+		String msg = driver.findElement(By.tagName("body")).getText();
+			
+		
+		if(msg.contains("The new customer has been added successfully"))
+		{
+			Assert.assertTrue(true);
+			logger.info("test case passed....");
+		}
+		else
+		{
+			captureScreen(driver,"addNewCustomer");
+			Assert.assertTrue(false);
+		}
+				
+	}
+
+	private String randomestring() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
